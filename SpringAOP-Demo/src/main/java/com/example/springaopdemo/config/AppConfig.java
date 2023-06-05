9c5b67e55d55ecd6ea7896cd13e04856c5f006bd
Cbiltps <1239669254@qq.com>
2023-06-06 05:31:51 +0800

    拦截器及添加url前缀实现

--- SpringAOP-Demo/src/main/java/com/example/springaopdemo/config/AppConfig.java
+++ SpringAOP-Demo/src/main/java/com/example/springaopdemo/config/AppConfig.java
@@ -2,7 +2,9 @@ package com.example.springaopdemo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -20,15 +22,25 @@ public class AppConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginIntercept)
-                .addPathPatterns("/**") // 拦截所有的url
-                .excludePathPatterns("/user/login") // 不拦截登录接口
-                .excludePathPatterns("/user/register")
-                .excludePathPatterns("/login.html") // 不拦截登录页面
-                .excludePathPatterns("/register.html")
-                .excludePathPatterns("/**/*.js") // 不拦截js文件
-                .excludePathPatterns("/**/*.css")
-                .excludePathPatterns("/**/*.png")
-                .excludePathPatterns("/**/*.jpg");
+//        registry.addInterceptor(loginIntercept)
+//                .addPathPatterns("/**") // 拦截所有的url
+//                .excludePathPatterns("/user/login") // 不拦截登录接口
+//                .excludePathPatterns("/user/register")
+//                .excludePathPatterns("/login.html") // 不拦截登录页面
+//                .excludePathPatterns("/register.html")
+//                .excludePathPatterns("/**/*.js") // 不拦截js文件
+//                .excludePathPatterns("/**/*.css")
+//                .excludePathPatterns("/**/*.png")
+//                .excludePathPatterns("/**/*.jpg");
+    }
+
+    /**
+     * 所有的接口添加 api 前缀
+     * @param configurer
+     */
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        // 其中第二个参数是一个表达式, 设置为 true 表示启动前缀
+        configurer.addPathPrefix("api", c -> true);
     }
 }
